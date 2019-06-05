7e1292093ab02417d8f0eca0455cf2845eb950ba
Alex Wellnitz <moin@wellnitz-alex.de>
2019-06-05 11:31:05 +0200

    Update emoji.js
    Add some comments

--- emoji.js
+++ emoji.js
@@ -1,5 +1,7 @@
-// Emojis
-// Define your custom commands and emoji
+// Emoji.js
+// Alex Wellnitz
+
+// Define Emojis
 var commands = [
     [ "unicorn", "🦄" ],
     [ "pizza", "🍕" ],
@@ -11,20 +13,21 @@ var commands = [
     [ "moon", "🌑"],
     [ "full-moon", "🌕"],
     [ "half-moon", "🌓"]
-  ];
+];
   
-  (function() {
-    if(!window.console) return;
-  
-    // Create custom commands
-    commands.forEach(function(command) {
-      window.console[command[0]] = function() {
-  
-        // Get arguments as a string
-        var args = Array.prototype.slice.call(arguments).toString().split(',').join(', ');
-  
-        // Log to the console with emoji
-        console.log(command[1] + "  " + args);
-      }
-    });
-  })();
+// console.emoji() Function
+(function() {
+if(!window.console) return;
+
+// Create custom commands
+commands.forEach(function(command) {
+  window.console[command[0]] = function() {
+
+    // Get arguments as a string
+    var args = Array.prototype.slice.call(arguments).toString().split(',').join(', ');
+
+    // Log to the console with emoji
+    console.log(command[1] + "  " + args);
+  }
+});
+})();
